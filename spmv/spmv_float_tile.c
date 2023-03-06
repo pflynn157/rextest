27c564050452a353b63621914df44be215283b70
patrickf2000 <dev@patrickflynn.co>
2023-03-06 13:04:09 -0500

    Added tiled programs

---
+++ spmv/spmv_float_tile.c
@@ -0,0 +1,102 @@
+////Example of sparse matrix-vector multiply, using CSR (compressed sparse row format).
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+// Add timing support
+#include <sys/timeb.h>
+
+#define REAL float
+
+double read_timer() {
+    struct timeb tm;
+    ftime(&tm);
+    return (double) tm.time + (double) tm.millitm / 1000.0;
+}
+
+
+//#define DEFAULT_DIMSIZE 256
+
+void print_array(char *title, char *name, REAL *A, int n, int m) {
+    fprintf(stderr, "%s:\n", title);
+    int i, j;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++) {
+            fprintf(stderr, "%s[%d][%d]:%f  ", name, i, j, A[i * m + j]);
+        }
+        fprintf(stderr, "\n");
+    }
+    fprintf(stderr, "\n");
+}
+
+/*  subroutine error_check (n,m,alpha,dx,dy,u,f)
+ implicit none
+ ************************************************************
+ * Checks error between numerical and exact solution
+ *
+ ************************************************************/
+
+int main(int argc, char *argv[]) {
+    int    *ia, *ja;
+    REAL *a, *x, *y;
+    int    row, i, j, idx, n, nnzMax, nnz, nrows;
+    REAL ts, t, rate;
+    n = 10240;
+    //n = 24;
+    if (argc > 1) n = atoi(argv[1]);
+
+    nrows  = n * n;
+    nnzMax = nrows * 5;
+    ia = (int*)malloc(nrows*sizeof(int));
+    ja = (int*)malloc(nnzMax*sizeof(int));
+    a  = (REAL*)malloc(nnzMax*sizeof(REAL));
+    /* Allocate the source and result vectors */
+    x = (REAL*)malloc(nrows*sizeof(REAL));
+    y = (REAL*)malloc(nrows*sizeof(REAL));
+
+    row = 0;
+    nnz = 0;
+    for (i=0; i<n; i++) {
+		for (j=0; j<n; j++) {
+			ia[row] = nnz;
+			if (i>0) { ja[nnz] = row - n; a[nnz] = -1.0; nnz++; }
+			if (j>0) { ja[nnz] = row - 1; a[nnz] = -1.0; nnz++; }
+			ja[nnz] = row; a[nnz] = 4.0; nnz++;
+			if (j<n-1) { ja[nnz] = row + 1; a[nnz] = -1.0; nnz++; }
+			if (i<n-1) { ja[nnz] = row + n; a[nnz] = -1.0; nnz++; }
+			row++;
+		}
+    }
+    ia[row] = nnz;
+
+    /* Create the source (x) vector */
+    for (i=0; i<nrows; i++) x[i] = 1.0;
+
+    double elapsed = read_timer();
+    
+    for (row=0; row<nrows; row++) {
+		REAL sum = 0.0;
+		#pragma omp tile sizes(2,2)
+		#pragma omp simd reduction(+:sum)
+		for (idx=ia[row]; idx<ia[row+1]; idx++) {
+			sum += a[idx] * x[ja[idx]];
+		}
+		y[row] = sum;
+    }
+    elapsed = read_timer() - elapsed;
+        
+    //printf("seq elasped time(s): %.4f\n", elapsed);
+  
+    int errors = 0;
+    for (row=0; row<nrows; row++) {
+		if (y[row] < 0) {
+			//fprintf(stderr,"y[%d]=%f, fails consistency test\n", row, y[row]);
+			++errors;
+		}
+    }
+    //printf("Errors: %d\n", errors);
+    
+    printf("%.4f", elapsed);
+    
+    free(ia); free(ja); free(a); free(x); free(y);
+    return 0;
+}
---
+++ sum/sum_float_tile.c
@@ -0,0 +1,92 @@
+//sum.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/timeb.h>
+#include <malloc.h>
+
+#define N_RUNS 20
+#define N 10240000
+
+// read timer in second
+double read_timer() {
+    struct timeb tm;
+    ftime(&tm);
+    return (double) tm.time + (double) tm.millitm / 1000.0;
+}
+
+//Create a matrix and a vector and fill with random numbers
+void init(float *X) {
+    for (int i = 0; i<N; i++) {
+        X[i] = (float)rand()/(float)(RAND_MAX/10.0);
+    }
+}
+
+//Our sum function- what it does is pretty straight-forward.
+float sum(float *X) {
+    float result = 0;
+    
+    #pragma omp tile sizes(2,2)
+    #pragma omp simd reduction(+:result)
+    for (int i = 0; i<N; i++) {
+        result += X[i];
+    }
+    
+    return result;
+}
+
+// Debug functions
+float sum_serial(float *X) {
+    float result = 0;
+    
+    for (int i = 0; i<N; i++) {
+        result += X[i];
+    }
+    
+    return result;
+}
+
+int main(int argc, char **argv) {
+    //Set everything up
+    float *X = malloc(sizeof(float)*N);
+    float result, result_serial;
+    
+    srand(time(NULL));
+    init(X);
+    
+    //warming up
+    result = sum(X);
+    result_serial = sum_serial(X);
+    
+    double t = 0;
+    double start = read_timer();
+    for (int i = 0; i<N_RUNS; i++) {
+        fprintf(stderr, "%d ", i);
+        result = sum(X);
+        fprintf(stderr, "(%f)", result);
+    }
+    fprintf(stderr, "\n");
+    t += (read_timer() - start);
+    
+    double t_serial = 0;
+    double start_serial = read_timer();
+    for (int i = 0; i<N_RUNS; i++)
+        result_serial = sum_serial(X);
+    t_serial += (read_timer() - start_serial);
+    
+    double gflops = ((2.0 * N) * N * N_RUNS) / (1.0e9 * t);
+    double gflops_serial = ((2.0 * N) * N * N_RUNS) / (1.0e9 * t_serial);
+    
+    /*printf("==================================================================\n");
+    printf("Performance:\t\t\tRuntime (s)\t GFLOPS\n");
+    printf("------------------------------------------------------------------\n");
+    printf("Sum (SIMD):\t\t%4f\t%4f\n", t/N_RUNS, gflops);
+    printf("Sum (Serial):\t\t%4f\t%4f\n", t_serial/N_RUNS, gflops_serial);
+    printf("Correctness check: %f\n", result_serial - result);*/
+    printf("%4f", t/N_RUNS);
+    
+    free(X);
+    
+    return 0;    
+}
+
