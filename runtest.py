d0b8511bd847b0812dd953b858645fbe3760508b
Patrick Flynn <pflynn157@gmail.com>
2023-03-31 10:33:33 -0400

    Arm tests update

--- runtest.py
+++ runtest.py
@@ -6,7 +6,7 @@ import os
 import sys
 
 #base_folders = [ "axpy", "sum", "matmul", "matvec", "spmv" ]
-base_folders = ["matmul", "matvec"]
+base_folders = ["spmv"]
 folders = list()
 
 for folder in base_folders:
@@ -32,7 +32,7 @@ for folder in base_folders:
 output_file = "results.csv"
 if len(sys.argv) > 1:
     output_file = "results_" + str(sys.argv[1]) + ".csv"
-max_count = 2
+max_count = 1
 #max_count = 10
 
 for folder in folders:
